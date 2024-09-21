179082e8e9080e9f5bae44457d70bff033529fc2
Windisley <windisleyl@gmail.com>
2024-09-21 16:25:11 -0300

    Estrutura html criada

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <div className="contimgs">
+        <div className='boximgs'>
+          <img src="/piramides.png" alt="Piramides do Egito" />
+        </div>
+        <div className='boximgs'>
+          <img src="/piramides.png" alt="Piramides do Egito" />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
